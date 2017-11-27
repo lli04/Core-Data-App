99113a4c6c659ab221fd3b0092bf0791fcfcdcfd
Lin Li <linli@Lins-MacBook-Pro.local>
2017-11-27 07:52:16 -0500

    Initial Commit

---
+++ Core Data App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Core Data App
+//
+//  Created by Lin Li on 11/27/17.
+//  Copyright © 2017 Lin Li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
